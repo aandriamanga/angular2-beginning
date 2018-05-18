95005813211c95154f9c15cbec3b95c97d2aa7bf
Unknown <a.tojo.antonio@gmail.com>
2018-05-18 15:51:12 +0200

    databindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
